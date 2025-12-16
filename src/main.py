430ee611a3290c4daadcc9cfe8d9340c05a502f5
shubh-man007 <randombro2077@gmail.com>
2025-12-17 03:41:16 +0530

    v3

--- src/main.py
+++ src/main.py
@@ -201,4 +201,4 @@ async def patient_websocket(websocket: WebSocket, patient_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True)
